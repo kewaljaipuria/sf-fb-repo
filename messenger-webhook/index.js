6429c6db090ce654c52b2bbacb047c7ad6d76164
kewaljaipuria <48791340+kewaljaipuria@users.noreply.github.com>
2020-02-10 16:59:20 +0530

    Update index.js

--- messenger-webhook/index.js
+++ messenger-webhook/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json());
 // const request = require('request');
 
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_SECRET;
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
